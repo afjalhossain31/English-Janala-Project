a02ec110d8c185722317abce83a06209ff7ab05b
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-05 03:14:24 +0600

    added load word data for vocabulary

--- script/script.js
+++ script/script.js
@@ -5,12 +5,12 @@ const loadLessons = async () => {
 }
 
 const loadLevelWords = (id) => {
-    // console.log(id);
+    // console.log(id); //Dynamic id 
     url = `https://openapi.programming-hero.com/api/level/${id}`;
     // console.log(url);
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayLevelWords(data.data))
+        .then(res => res.json())
+        .then(data => displayLevelWords(data.data))
 }
 
 const displayLevelWords = (words) => {
@@ -18,19 +18,34 @@ const displayLevelWords = (words) => {
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = " "; //inner html empty
 
+    // {
+    //     "id":54,
+    //     "word":"Eager",
+    //     "meaning":"আগ্রহী",
+    //     "pronunciation":"ইগার",
+    //     "level_no":1
+    // }
+
     words.forEach(word => {
         console.log(word);
         const cardDiv = document.createElement("div");
         cardDiv.innerHTML = `
-        <p>Cat</p>
+            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold ">Meaning / Pronunciation</p>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
 
         `;
         wordContainer.appendChild(cardDiv);
     });
-    
+
 
 }
-
 
 const displayLessons = (lessons) => {
     // 1. Get the container and empty 
