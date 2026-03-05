e12291837024e8d8a82f68fe43f84c5df4d43fe9
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-06 01:33:47 +0600

    added completed

--- script/script.js
+++ script/script.js
@@ -1,162 +1,204 @@
-// create element for synonym / synonym er jonno element create korar jonno function
-const createElement = (Array) => {
-    // console.log(Array);
-    const htmlElement = Array.map((el) => `<span class="btn">${el}</span>`);
-    return(htmlElement.join(" "));
-} 
-
-const manageSpinner =(status) => {
-    if(status === true){
-        document.getElementById("spinner").classList.remove("hidden");
-        document.getElementById("word-container").classList.add("hidden");
-    } else {
-        document.getElementById("word-container").classList.add("hidden");
-        document.getElementById("spinner").classList.remove("hidden");
-    }
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join(" ");
+};
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
 }
+
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
 
 const loadLessons = () => {
-    fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then(res => res.json())  //promise of json data
-        .then((json) => displayLessons(json.data)) //json / data jekono kisho
-}
+  fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then((res) => res.json()) // promise of json data
+    .then((json) => displayLesson(json.data));
+};
 
-// load level words and active button / remove active class from all
-const loadLevelWords = (id) => {
-   manageSpinner(true);
-    // console.log(id); //Dynamic id 
-    url = `https://openapi.programming-hero.com/api/level/${id}`;
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            removeActive();// remove active class from all buttons
-            const clickBtn = document.getElementById(`lesson-btn-${id}`);
-            // console.log(clickBtn);
-            clickBtn.classList.add("btn-active"); // add active class to the clicked button
-            displayLevelWords(data.data)
-        })
-}
-
-// remove active class from all buttons
 const removeActive = () => {
-    const lessonButton = document.querySelectorAll(".lesson-btn");
-    // lessonButton.forEach((btn) => btn.classList.remove("btn-active")); //shortcut
-    lessonButton.forEach(btn => {
-        btn.classList.remove("btn-active");
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  //   console.log(lessonButtons);
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
+const loadLevelWord = (id) => {
+  manageSpinner(true);
+
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActive(); // remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active"); // add active class
+      displayLevelWord(data.data);
     });
-}
+};
 
+// {
+//     "word": "Linger",
+//     "meaning": "থেমে থাকা / বিলম্ব করা",
+//     "pronunciation": "লিঙ্গার",
+//     "level": 2,
+//     "sentence": "She lingered at the door, unwilling to leave.",
+//     "points": 2,
+//     "partsOfSpeech": "verb",
+//     "synonyms": [
+//         "stay",
+//         "remain",
+//         "delay"
+//     ],
+//     "id": 12
+// }
 
-
-// load word details / then bade async use korlam
 const loadWordDetail = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/word/${id}`;
-    const res = await fetch(url);
-    const details = await res.json();
-    // console.log(details);
-    displayWordDetail(details.data);
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
 
-}
-const displayWordDetail = (word) => {
-    console.log(word);
-    const detailBox = document.getElementById("details-container");
-    // detailBox.innerHTML = " hi i am from js";
-    detailBox.innerHTML = `
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+    <div class="">
+            <h2 class="text-2xl font-bold">
+              ${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${
+    word.pronunciation
+  })
+            </h2>
+          </div>
+          <div class="">
+            <h2 class="font-bold">Meaning</h2>
+            <p>${word.meaning}</p>
+          </div>
+          <div class="">
+            <h2 class="font-bold">Example</h2>
+            <p>${word.sentence}</p>
+          </div>
+          <div class="">
+            <h2 class="font-bold">Synonym</h2>
+            <div class="">${createElements(word.synonyms)}</div>
+          </div>
     
-     <div class="">
-                    <h2 class="text-2xl font-bold">${word.word}(<i class="fa-solid fa-microphone-lines"></i> : ${word.pronunciation})</h2>
-                </div>
-                <div class="font-bold">Meaning</div>
-                <p>${word.meaning}</p>
+    
+    `;
+  document.getElementById("word_modal").showModal();
+};
 
-                <div class="">
-                    <h2 class="font-bold">Example</h2>
-                    <p>${word.sentence}</p>
-                </div>
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
 
-                <div class="">
-                    <h2 class="font-bold">Synonym</h2>
-                    <div class="flex gap-2 flex-wrap">
-                        ${createElement(word.synonyms)}
-                    </div>
-                </div>
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div
+        class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla"
+      >
+        <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+    
+    `;
+    manageSpinner(false);
+    return;
+  }
 
+  //   {
+  //     "id": 82,
+  //     "level": 1,
+  //     "word": "Car",
+  //     "meaning": "গাড়ি",
+  //     "pronunciation": "কার"
+  // }
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div
+        class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
+      >
+        <h2 class="font-bold text-2xl">${
+          word.word ? word.word : "শব্দ পাওয়া যায়নি"
+        }</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="text-2xl font-medium font-bangla">"${
+          word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"
+        } / ${
+      word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"
+    }"</div> 
+        <div class="flex justify-between items-center">
+          <button onclick="loadWordDetail(${
+            word.id
+          })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button onclick="pronounceWord('${
+            word.word
+          }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
+    `;
+    wordContainer.append(card);
+  });
+  manageSpinner(false);
+};
+
+const displayLesson = (lessons) => {
+  //   1. get the container & empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  //   2. get into evey lessons
+  for (let lesson of lessons) {
+    //         3. create Element
+    // console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+                 <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-outline btn-primary lesson-btn">
+                 <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+                  </button>
     `;
 
-    document.getElementById("word_modal").showModal();
+    //         4. append into container
 
-}
-
-
-
-const displayLevelWords = (words) => {
-    // console.log(words);
-    const wordContainer = document.getElementById("word-container");
-    wordContainer.innerHTML = " "; //inner html empty
-
-    if (words.length === 0) {
-        wordContainer.innerHTML = `
-        <div class="text-center col-span-full rounded-xl space-y-6 font-bangla py-10 px-5">
-
-        <img src="./assets/alert-error.png" class="w-20 mx-auto" alt="">
-
-            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
-            <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
-        </div>
-
-        `;
-        manageSpinner(false);
-        return;
-    }
-
-    // {
-    //     "id":54,
-    //     "word":"Eager",
-    //     "meaning":"আগ্রহী",
-    //     "pronunciation":"ইগার",
-    //     "level_no":1
-    // }
-
-    words.forEach(word => {
-        console.log(word);
-        const cardDiv = document.createElement("div");
-        cardDiv.innerHTML = `
-            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4 flex flex-col justify-between h-full">
-            <div class="space-y-4">
-            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
-            <p class="font-semibold ">Meaning / Pronunciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "প্রনুন্সিয়েশন পাওয়া যায়নি"}"</div>
-            </div>
-            <div class="flex justify-between items-center">
-
-            <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
-            </div>
-        </div>
-
-        `;
-        wordContainer.appendChild(cardDiv);
-    });
-    manageSpinner(false);
-    
-}
-
-
-const displayLessons = (lessons) => {
-    // 1. Get the container and empty 
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML = " ";
-    // 2. get into every lessons 3. create Element 4. append child
-    for (let lesson of lessons) {
-        console.log(lesson);
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
-          <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWords(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"> <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
-
-        `;
-        levelContainer.appendChild(btnDiv);
-    }
-}
+    levelContainer.append(btnDiv);
+  }
+};
 
 loadLessons();
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  removeActive();
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  console.log(searchValue);
+
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWords = data.data;
+      console.log(allWords);
+      const filterWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue)
+      );
+
+      displayLevelWord(filterWords);
+    });
+});
